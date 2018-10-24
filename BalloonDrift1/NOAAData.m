8b7710bd28ee8165a72339acb9a73e15a9fa8fc6
Matthew Popplewell <mpopplew@purdue.edu>
2018-10-24 18:33:05 -0400

    NOAA Data file

---
+++ BalloonDrift1/NOAAData.m
@@ -0,0 +1,25 @@
+%% Purdue Orbital 
+% 
+% NOAA Data
+%
+% 
+% Created: October 24, 2018
+
+%% Description of model
+% 
+% Exports wind speed and direction given inputs of altitude, latitude, and
+% longitude from a pre-loaded NOAA .csv file.
+% 
+
+%% Function
+function [windSpeed, windDir] = NOAAData(alt,lat, lon)
+% inputs :
+% alt: altitude (m)
+% lat: latitude
+% lon: longitude
+
+% outputs: 
+% windSpeed: wind speed (m/s)
+% windDir: wind direction (degrees from N)
+
+%% Initializations
